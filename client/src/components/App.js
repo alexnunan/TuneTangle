dd2265dc3369c8f5327d55387e061beb5a2ace78
Alex Nunan <nunanamous21@gmail.com>
2023-06-09 13:15:06 -0400

    Fix Import

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import "../assets/scss/main.scss";
 import RegistrationForm from "./registration/RegistrationForm";
 import SignInForm from "./authentication/SignInForm";
 import TopBar from "./layout/TopBar";
-import PlaylistSelection from "./PlaylistSelection"
+import PlaylistSelection from "./playlistSelection"
 import LoginPage from "./LoginPage";
 import Home from "./Home"
 
